54fff3a19aa1a875db534dc8b99532a0bcbe8daa
Grapeka <chotwilaiwan@gmail.com>
2023-04-30 20:45:38 +0700

    `feat` : target by username backend

--- server/server.ts
+++ server/server.ts
@@ -5,45 +5,55 @@ import { Server as SocketIOServer, Socket } from 'socket.io';
 interface User {
   id: string;
   socket: Socket;
+  username?: string;
 }
 
 const app: Express = express();
 const http: Server = new Server(app);
 const io: SocketIOServer = new SocketIOServer(http, {
   cors: {
-    origin: '*', // Allow all origins
+    origin: '*',
     methods: ['GET', 'POST'],
   },
 });
 
-const connectedUsers: User[] = [];
+const users: User[] = [];
 
 io.on('connection', (socket: Socket) => {
   console.log(`Socket ${socket.id} connected`);
 
   const user: User = { id: socket.id, socket };
-  connectedUsers.push(user);
+  users.push(user);
 
-  socket.on('chat message', (data: { to: string; message: string }) => {
-    const targetUser: User | undefined = connectedUsers.find(
-      (user) => user.id === data.to
-    );
-    if (targetUser) {
-      targetUser.socket.emit('chat message', {
-        from: socket.id,
-        message: data.message,
-      });
-    }
+  socket.on('register', (username: string) => {
+    user.username = username;
+    users.push(user);
+    console.log('All users: ', users);
   });
+
+  socket.on(
+    'chat message',
+    (data: { from: string; to: string; message: string }) => {
+      const targetUser: User | undefined = users.find(
+        (user) => user.username === data.to
+      );
+
+      if (targetUser) {
+        targetUser.socket.emit('chat message', {
+          socketId: socket.id,
+          fromUser: data.from,
+          message: data.message,
+        });
+      }
+    }
+  );
 
   socket.on('disconnect', () => {
     console.log(`Socket ${socket.id} disconnected`);
 
-    const index: number = connectedUsers.findIndex(
-      (user) => user.socket === socket
-    );
+    const index: number = users.findIndex((user) => user.socket === socket);
     if (index !== -1) {
-      connectedUsers.splice(index, 1);
+      users.splice(index, 1);
     }
   });
 });
