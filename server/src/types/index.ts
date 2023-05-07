d896dc2b9c2345cb88166ea48fecce7bed590a6b
Grapeka <chotwilaiwan@gmail.com>
2023-05-07 12:24:27 +0700

    `feat` : create interfaces

---
+++ server/src/types/index.ts
@@ -0,0 +1,29 @@
+export interface IUser {
+  user_id: string;
+  name: string;
+  username: string;
+  hashed_password: string;
+  salt: string;
+  profile_image?: string;
+}
+
+export interface IChat {
+  chat_id: string;
+  userA_id: string;
+  userB_id: string;
+  created_at: Date;
+}
+export interface IMessageType {
+  message_type_id: string;
+  type_name: string;
+}
+
+export interface IMessage {
+  message_id: string;
+  chat_id: string;
+  sender_id: string;
+  receiver_id: string;
+  message_type_id: string;
+  content: string;
+  created_at: Date;
+}
