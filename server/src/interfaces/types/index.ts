a86d9f3316685d7711587bd793d28fd3366ce4a8
Grapeka <chotwilaiwan@gmail.com>
2023-06-05 11:04:22 +0700

    `refactor` : update interfaces, create participant

--- server/src/interfaces/types/index.ts
+++ server/src/interfaces/types/index.ts
@@ -1,29 +1,29 @@
 export interface IUser {
-  user_id: string;
+  user_id?: string;
   name: string;
   username: string;
+  public_key: string;
   hashed_password: string;
   salt: string;
   profile_image: string | null;
 }
 
 export interface IChat {
-  chat_id: string;
-  userA_id: string;
-  userB_id: string;
-  created_at: Date;
+  chat_id?: string;
+  name: string;
+  created_at?: Date;
 }
-export interface IMessageType {
-  message_type_id: string;
-  type_name: string;
+
+export interface IParticipant {
+  participant_id?: string;
+  chat_id: string;
+  user_id: string;
 }
 
 export interface IMessage {
-  message_id: string;
+  message_id?: string;
   chat_id: string;
   sender_id: string;
-  receiver_id: string;
-  message_type_id: string;
   content: string;
-  created_at: Date;
+  created_at?: Date;
 }
